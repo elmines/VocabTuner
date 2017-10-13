f7e2f26aadd7511ff9f1942d3a2fbe42bd33be07
Ethan Mines <elmines@crimson.ua.edu>
2017-10-13 12:28:15 -0500

    Clean punctuation from EuroParl corpus

--- scripts/europarse.py
+++ scripts/europarse.py
@@ -1,14 +1,62 @@
-numSentences = 50000
+numSentences = 25
 sourceLangPath = "../corpora/europarl-v7.es-en.es"
 destLangPath = "../corpora/europarl-v7.es-en.en"
 ctfPath = "../data/euroParles-en.ctf"
 
+sourceColumn = "S0"
+destColumn = "S1"
+EMPTY = ""
+
 source = open(sourceLangPath, "r")
 dest = open(destLangPath, "r")
-ctfPath = open(ctfPath, "w")
+ctf = open(ctfPath, "w")
+
+def cleanToken(token):
+    chars = []
+    for char in token:
+        if char.isalpha(): chars.append(char.lower())
+        elif char.isdigit(): chars.append(char)
+
+    if len(chars) > 0: return EMPTY.join(chars)
+    else:              return EMPTY               #Case in which we have just non alphanumeric characters by themselves?
+
+def writeSequence(sourceLine, destLine, seqId):
+   sourceTokens = sourceLine.split()
+   destTokens = destLine.split()
+
+   i = 0
+   while i < len(sourceTokens) and i < len(destTokens):
+       sourceToken = cleanToken(sourceTokens[i])
+       destToken = cleanToken(destTokens[i])
+
+       row = str(seqId) + ' |' + sourceColumn + " " + sourceToken + " |" + destColumn + " " + destToken + "\n"
+       ctf.write(row)
+
+       i += 1
+
+   while i < len(sourceTokens):
+       sourceToken = cleanToken(sourceTokens[i])
+       row = str(seqId) + ' |' + sourceColumn + " " + sourceToken + "\n"
+       ctf.write(row)
+       i += 1
+
+   while i < len(destTokens):
+       destToken = cleanToken(destTokens[i])
+       row = str(seqId) + ' |' + destColumn + " " + destToken + "\n"
+       ctf.write(row)
+       i += 1
+
 
 sourceLine = source.readline()
 destLine = dest.readline()
 
-while sourceLine and destLine:
-    
+seqId = 0
+while (seqId < numSentences) and sourceLine and destLine:
+    writeSequence(sourceLine, destLine, seqId)
+    seqId += 1 
+    sourceLine = source.readline()
+    destLine = dest.readline()
+
+source.close()
+dest.close()
+ctf.close()
