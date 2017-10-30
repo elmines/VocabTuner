14ad54723d1b65dba9cc05c6fb7950e824736967
elmines <elmines325@gmail.com>
2017-10-30 09:29:20 -0500

    WIP: Actually store the 'unknown value' fields in one2one's dictionaries

--- scripts/one2one.py
+++ scripts/one2one.py
@@ -9,27 +9,42 @@ class Set(Enum):
 class one2one(MutableMapping):
     x2y = None
     y2x = None
-    unknown_val = None
+    unknown_x = None
+    unknown_y = None
 
     @staticmethod
     def __key_not_found(key):
         return str(key) + " not in domain or codomain."
 
-    def __init__(self, unknown_val = None):
+    @staticmethod
+    def __breaks_mapping():
+        return "Cannot alter unknown_x <--> unknown_y mapping."
+
+    def __init__(self, unknown_x = None):
         self.x2y = {}
         self.y2x = {}
-        self.unknown_val = unknown_val
+
+        if unknown_x and unknown_y:
+            self.unknown_x = unknown_x
+            self.unknown_y = unknown_y
+            self.x2y[unknown_x] = unknown_y
+            self.y2x[unknown_y] = unknown_x
+        elif unknown_x or unknown_y:
+            raise ValueError("One must specify both unknown_x and unknown_y or neither.")
 
     def __getitem__(self, key):
         if key in self.x2y:
             return self.x2y[key]
         if key in self.y2x:
             return self.y2x[key]
-        if self.unknown_val:
-            return self.unknown_val
+        if self.unknown_x:
+            return self.unknown_x
         raise KeyError(one2one.__key_not_found(key))
 
     def __setitem__(self, key, value):
+      #Checked, and "in" does indeed perform a deep equality comparison
+      if self.unknown_x in (key, value) or self.unknown_y in (key, value):
+           raise ValueError(one2one.__breaks_mapping())
       if key in self.x2y:
           del self.y2x[ self.x2y[key] ]
       elif key in self.y2x:
@@ -38,6 +53,8 @@ class one2one(MutableMapping):
       self.y2x[value] = key
 
     def __delitem__(self, key):
+        if key == self.unknown_x or key == self.unknown_y:
+           raise ValueError(one2one.__breaks_mapping())
         if key in self.x2y:
             value = self.x2y[key]
             del self.x2y[key]
