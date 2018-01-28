425947eb507183a57908309327f98238b98e6e59
Ethan L. Mines - University of Alabama <ualelm@uv.asc.edu>
2018-01-28 09:01:57 -0600

    Get training command line options right

--- master.sh
+++ master.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
 
-SOURCE_LANG=$1
-DEST_LANG=$2
+COMMAND=$1
+SOURCE_LANG=$2
+DEST_LANG=$3
 
-DATA_ROOT=data
-DATA_DIR=$DATA_ROOT/$SOURCE_LANG-$DEST_LANG
+#Commands
+ALL="all"
+PREPROCESS="preprocess"
+TRAIN="train"
 
-if [ ! -e $DATA_DIR ]
+
+
+EXPERIMENT=`pwd`/experiments/$SOURCE_LANG-$DEST_LANG
+
+
+if [ $COMMAND = $ALL ] || [ $COMMAND = $PREPROCESS ]
 then
-  DATA_DIR=$DATA_ROOT/$DEST_LANG-$SOURCE_LANG
-
-  if [ ! -e $DATA_DIR ]
-  then
-    echo "Error: no data for that language pair $SOURCE_LANG-$DEST_LANG"
-    exit 1
-  fi
+    ./preprocess.sh $EXPERIMENT/data $SOURCE_LANG $DEST_LANG
 fi
 
-#Preprocess command
-    #Tokenize input data
-    #Truecase input data
-    #Learn BPE segments from input data
-    #Apply BPE to input data
-
-#Train command
-    #Set up Marian model
-    #Train model
-
-#Evaluation command
-    #Preprocess test data
-    #Translate test data
-    #Undo BPE merges on test data
-    #Undo Truecasing on test data
-    #Undo tokenization on test data
-
-    #Wrap test data in SGML
-    #Evaluate test data against actual corpus 
+if [ $COMMAND = $ALL ] || [ $COMMAND = $TRAIN ]
+then
+     ./train.sh $EXPERIMENT $SOURCE_LANG $DEST_LANG
+fi
--- train.sh
+++ train.sh
@@ -18,15 +18,19 @@ DEV_DEST=$EXPERIMENT/$DATA_DIR/${DEST_LANG}.dev.tok.tc.bpe
 JOINT_VOCAB=$EXPERIMENT/$DATA_DIR/${SOURCE_LANG}-${DEST_LANG}.yml
 
 #Models
-MODELS=$EXPERIMENT/$MODEL_DIR/
+MODELS=$EXPERIMENT/$MODEL_DIR/${SOURCE_LANG}-${DEST_LANG}.npz
 
 #Training Configuration
-CONFIG=$EXPERIMENT/$DATA_DIR/${SOURCE_LANG}-${DEST_LANG}.config.yml
+CONFIG=$EXPERIMENT/${SOURCE_LANG}-${DEST_LANG}.config.yml
 
 #Logging
-TRAIN_LOG=$EXPERIMENT/$DATA_DIR/${SOURCE_LANG}-${DEST_LANG}.train.log
-DEV_LOG=$EXPERIMENT/$DATA_DIR/${SOURCE_LANG}-${DEST_LANG}.dev.log
+TRAIN_LOG=$EXPERIMENT/$LOG_DIR/${SOURCE_LANG}-${DEST_LANG}.train.log
+DEV_LOG=$EXPERIMENT/$LOG_DIR/${SOURCE_LANG}-${DEST_LANG}.dev.log
 
+echo "MODELS=" $MODELS
+echo "CONFIG=" $CONFIG
+echo "TRAIN_LOG=" $TRAIN_LOG
+echo "DEV_LOG=" $DEV_LOG
 
 if [ ! -e $CONFIG ]
 then
@@ -37,14 +41,14 @@ then
         --train-sets $TRAIN_SOURCE $TRAIN_DEST \
         --valid-sets $DEV_SOURCE   $DEV_DEST \
         --vocabs $JOINT_VOCAB $JOINT_VOCAB \
-        --model  $MODEL \
+        --model  $MODELS \
         --enc-depth 4 --enc-type alternating --enc-cell lstm --enc-cell-depth 2 \
         --dec-depth 4                        --dec-cell lstm --dec-cell-base-depth 4 --dec-cell-high-depth 2 \
         --tied-embeddings \
         --layer-normalization \
         --skip \
-        --mini-batch-fit \
-        --workspace 6500 \
+        --mini-batch 64 \
+        --workspace 8192 \
         --dropout-rnn 0.2 --dropout-src 0.1 --exponential-smoothing \
         --early-stopping 5 --disp-freq 1000 \
         --device 0 \
@@ -52,4 +56,4 @@ then
         --dump-config > $CONFIG
 fi
 
-~/marian/build/marian --config $CONFIG
+#~/marian/build/marian --config $CONFIG
