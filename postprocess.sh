44d9554d3c6e414367dc988fa20767b168a74242
Ethan L. Mines - University of Alabama <ualelm@dmcvlogin1.asc.edu>
2018-01-30 09:00:08 -0600

    WIP: Set up evaluation workflow

--- postprocess.sh
+++ postprocess.sh
@@ -7,12 +7,16 @@ RAW_TRANSLATION=$2
 SOURCE=$3
 REFERENCE=$4
 
+SYSTEM="TheUniversityOfAlabama"
+
+SGML_TRANSLATION=${RAW_TRANSLATION}.sgm
+#SGML_TRANSLATION=output.txt
 
 cat $RAW_TRANSLATION | \
     sed -r 's/(@@ )|(@@ ?$)//g' | \
-    ~mosesdecoder/scripts/recaser/detruecase.perl | \
-    ~mosesdecoder/scripts/tokenizer/detokenizer.perl | \
-    ~/scripts/wrap-xml.perl $DEST_LANG $REFERENCE | \
+    ~/mosesdecoder/scripts/recaser/detruecase.perl | \
+    ~/mosesdecoder/scripts/tokenizer/detokenizer.perl | \
+    ~/scripts/wrap-xml.perl $DEST_LANG $REFERENCE $SYSTEM \
     > $SGML_TRANSLATION
     
-perl ~/scripts/mteval-v14.perl -r $REFERENCE -s $SOURCE -t $SGML_TRANSLATION
+perl ~/scripts/mteval-v14.pl  -s $SOURCE -r $REFERENCE -t $SGML_TRANSLATION
--- translate.sh
+++ translate.sh
@@ -5,18 +5,18 @@ module load boost/1.58.0 cuda/8.0.44
 SOURCE_LANG=es
 DEST_LANG=en
 
-EXPERIMENT=~/VocabTuner/experiments/es-en
+EXPERIMENT=~/VocabTuner/experiments/${SOURCE_LANG}-${DEST_LANG}
 
-MODEL=$EXPERIMENT/models/es-en.iter90000.npz
+MODEL=$EXPERIMENT/models/es-en.npz
 JOINT_VOCAB=$EXPERIMENT/data/es-en.yml
 
 
-SOURCE=$EXPERIMENT/data/${SOURCE_LANG}.test
-TRANSLATION=$EXPERIMENT/data/${DEST_LANG}.test.trans
+SOURCE=$EXPERIMENT/"eval"/${SOURCE_LANG}.test.tok.tc.bpe
+TRANSLATION=$EXPERIMENT/"eval"/${DEST_LANG}.trans
 
 if [ ! -e $TRANSLATION ]
 then
-  marian-decoder \
+  ~/marian/build/marian-decoder \
     --models $MODEL \
     --vocabs $JOINT_VOCAB $JOINT_VOCAB \
     --input $SOURCE \
