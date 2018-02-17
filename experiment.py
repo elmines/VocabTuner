f931f0fc2258d3ba7bb0e6fe63cd09f162e4aa14
Ethan L. Mines - University of Alabama <ualelm@uv.asc.edu>
2018-02-17 07:46:41 -0600

    Score translation

--- experiment.py
+++ experiment.py
@@ -19,11 +19,9 @@ if subword_nmt not in sys.path:
 from apply_bpe import BPE
 
 
-SKIP_PREPROC = True
-SKIP_TRAIN = True
-
-def f():
-    print("Hello, world!")
+#SKIP_PREPROC = True
+#SKIP_TRAIN = True
+#SKIP_TRANS = True
 
 def preprocess(source_lang, dest_lang):
     experiment_dir = os.path.abspath( os.path.join("experiments", source_lang + "-" + dest_lang) )
@@ -47,6 +45,8 @@ class Experiment:
     source_codes = None
     dest_codes = None
     vocab_prefix = None
+
+    verbose = True
 
     max_merges = 100000 #Constant for now
 
@@ -83,12 +83,14 @@ class Experiment:
                        dest_lang = "en",
                        joint_codes = None,
                        source_codes = None, dest_codes = None,
-                       model_prefix = None, train_log_prefix = None, vocab_dir = None, translation_dir = None):
+                       model_prefix = None, train_log_prefix = None, vocab_dir = None, translation_dir = None,
+                       verbose = True):
         """
         dest_lang specifies the target language when using Moses's wrap_xml.perl
         Either joint_vocab must be specified, or both source_vocab and dest_vocab must be specified.
         """
 
+        self.verbose = verbose
         self.train_source = os.path.abspath(train_source)
         self.train_dest = os.path.abspath(train_dest)
         self.dev_source = os.path.abspath(dev_source)
@@ -108,18 +110,24 @@ class Experiment:
 
 
         self.model_prefix = Experiment.__process_prefix(model_prefix, "model", Experiment.__model_extension())
+        if self.verbose: print("Set model prefix as %s" % str(self.model_prefix))
+
         self.train_log_prefix = Experiment.__process_prefix(model_prefix, "train", Experiment.__log_extension())
+        if self.verbose: print("Set training log prefix as %s" % str(self.train_log_prefix))
 
         if not(vocab_dir):
             self.vocab_dir = os.path.abspath( os.path.join(".") )
+            if self.verbose: print("Set working directory as vocab directory.")
         else:
             self.vocab_dir = os.path.abspath( vocab_dir )
 
         if not(translation_dir):
             self.translation_dir = os.path.abspath( os.path.join(".") )
+            if self.verbose: print("Set working directory as translation directory.")
         else:
             self.translation_dir = os.path.abspath( translation_dir )
 
+    @staticmethod
     def parse_nist(report):
         scoreLabel = "NIST score = "
         maxRawScore = 100
@@ -143,27 +151,16 @@ class Experiment:
     def score_nist(self, model, source_vocab, dest_vocab, bpe_dev_source, num_merges):
         sgm_translation = os.path.abspath( os.path.join(self.translation_dir, self.dest_lang + ".s" + str(num_merges) + ".tst.sgm") )
 
-        print("Translating development set with %d merges and writing to %s" % (num_merges, sgm_translation))
-
-        #Good
         translate_command = ["shell/translate.sh", str(model), str(source_vocab), str(dest_vocab), str(bpe_dev_source)]
         translating = subprocess.Popen( translate_command, universal_newlines=True, stdout=subprocess.PIPE)
-
-        #status = translating.wait()
-        #if status:
-        #    raise RuntimeError("Translation process with " + str(num_merges) + " merges failed with exit code " + str(status)) 
-
-        #FIXME: Need to get the proper path for dev_source_sgm
         with open(sgm_translation, "w") as out:
             postprocess_command = ["shell/postprocess.sh", self.dest_lang, str(self.dev_source)]
             postprocessing = subprocess.Popen( postprocess_command, universal_newlines=True, stdin=translating.stdout, stdout=out)
             status = postprocessing.wait()
-        if status:
-            raise RuntimeError("Translation process with " + str(num_merges) + " merges failed with exit code " + str(status)) 
+            if status:
+                raise RuntimeError("Translation process with " + str(num_merges) + " merges failed with exit code " + str(status)) 
 
-        print("Finished translating successfully")
-
-        exit(0)
+        if self.verbose: print("Wrote translation %s" % (sgm_translation))
 
         score_command = ["perl", "/home/ualelm/scripts/mteval-v14.pl", "-s", str(self.dev_source), "-t", str(sgm_translation), "-r", str(self.dev_dest)]
         scoring = subprocess.Popen(score_command, universal_newlines=True, stdout=subprocess.PIPE)
@@ -172,12 +169,7 @@ class Experiment:
         if status:
             raise RuntimeError("Scoring process with " + str(num_merges) + " merges failed with exit code " + str(status)) 
 
-
-        print("Finished scoring process successfully")
-        print(output)
-        exit(0)
-
-        return parse_nist(output)
+        return Experiment.parse_nist(output)
 
     @staticmethod
     def __preprocess_corpus(raw, processed, bp_encoder):
@@ -191,10 +183,6 @@ class Experiment:
            source_vocab = os.path.join(self.vocab_dir, "joint.s%d" % num_merges)
            dest_vocab = source_vocab
 
-           if SKIP_PREPROC: return (source_vocab, dest_vocab)
-
-           #print("source_vocab = %s" % source_vocab)
-
            cat_command = ["cat", str(bpe_train_source), str(bpe_train_dest)]
            cat_proc = subprocess.Popen(cat_command, stdout = subprocess.PIPE, universal_newlines=True)
 
@@ -206,11 +194,11 @@ class Experiment:
            if status:
                raise RuntimeError("Generating joint vocabulary with " + str(num_merges) + " merges failed with exit code " + str(status))
 
+           if self.verbose: print("Wrote joint vocabulary file %s" % source_vocab)
+
        else:
            source_vocab = os.path.join(self.vocab_dir, "source.s%d.vocab" % num_merges)
            dest_vocab = os.path.join(self.vocab_dir, "dest.s%d.vocab" % num_merges)
-
-           if SKIP_PREPROC: return (source_vocab, dest_vocab)
 
            vocab_proc = subprocess.Popen(["marian/build/marian-vocab"], universal_newlines=True, stdin=bpe_train_source, stdout=source_vocab)
            status = vocab_proc.wait()
@@ -222,6 +210,8 @@ class Experiment:
            if status:
               raise RuntimeError("Generating dest vocabulary with " + str(num_merges) + " merges failed with exit code " + str(status))
 
+           if self.verbose: print("Wrote vocabulary files %s, %s" % (source_vocab, dest_vocab))
+
        return (source_vocab, dest_vocab)
 
 
@@ -230,54 +220,33 @@ class Experiment:
        bpe_train_dest = os.path.join( str(self.train_dest) + ".s" + str(num_merges))
 
        bpe_dev_source = os.path.join( str(self.dev_source_preproc) + ".s" + str(num_merges))
-       bpe_dev_dest = os.path.join( str(self.dev_dest) + ".s" + str(num_merges))
+       bpe_dev_dest = os.path.join( str(self.dev_dest) + ".s" + str(num_merges))             #FIXME: We don't actually need this corpus
 
-       toReturn = (bpe_train_source, bpe_train_dest, bpe_dev_source, bpe_dev_dest)
-       #print("File paths for %d merges:" %  num_merges, str(toReturn))
+       with open(self.source_codes, "r") as src_codes:
+           source_encoder = BPE(src_codes, num_merges)
 
-       if not SKIP_PREPROC:
-           #print("source_codes =", self.source_codes)
-           with open(self.source_codes, "r") as src_codes:
-               source_encoder = BPE(src_codes, num_merges)
-               Experiment.__preprocess_corpus(self.train_source, bpe_train_source, source_encoder)
-               print("Wrote %s" % str(bpe_train_source))
-               Experiment.__preprocess_corpus(self.dev_source, bpe_dev_source, source_encoder)
-               print("Wrote %s" % str(bpe_dev_source))
+           Experiment.__preprocess_corpus(self.train_source, bpe_train_source, source_encoder)
+           if self.verbose: print("Wrote segmented training source corpus to %s" % str(bpe_train_source))
 
+           Experiment.__preprocess_corpus(self.dev_source_preproc, bpe_dev_source, source_encoder)
+           if self.verbose: print("Wrote segmented training destination corpus to %s" % str(bpe_dev_source))
 
-           with open(self.dest_codes, "r") as dst_codes:
-               dest_encoder = BPE(dst_codes, num_merges)
-               Experiment.__preprocess_corpus(self.train_dest, bpe_train_dest, dest_encoder)
-               print("Wrote %s" % str(bpe_train_dest))
-               #Experiment.__preprocess_corpus(self.dev_dest, bpe_dev_dest, dest_encoder)
-               #print("Wrote %s" % str(bpe_dev_dest))
+       with open(self.dest_codes, "r") as dst_codes:
+           dest_encoder = BPE(dst_codes, num_merges)
 
+           Experiment.__preprocess_corpus(self.train_dest, bpe_train_dest, dest_encoder)
+           if self.verbose: print("Wrote segmented development source corpus to %s" % str(bpe_train_dest))
 
-       #(source_vocab, dest_vocab) = self.__generate_vocabs(bpe_dev_source, bpe_dev_dest, num_merges)
        (source_vocab, dest_vocab) = self.__generate_vocabs(bpe_train_source, bpe_train_dest, num_merges)
 
+       return (bpe_train_source, bpe_train_dest, bpe_dev_source, bpe_dev_dest, source_vocab, dest_vocab)
 
-       toReturn = (bpe_train_source, bpe_train_dest, bpe_dev_source, bpe_dev_dest, source_vocab, dest_vocab)
-
-       return toReturn
 
     def vocab_rating(self, num_merges):
          model_path = Experiment.detailed_path(self.model_prefix, num_merges[0], Experiment.__model_extension())
          log_path = Experiment.detailed_path(self.train_log_prefix, num_merges[0], Experiment.__log_extension())
 
-         #print("num_merges = %s" % num_merges[0])
-         #print("model_path = %s" % model_path)
-         #print("log_path = %s" % log_path)
-
-
-         #print("About to call preprocess_corpora")
          (bpe_train_source, bpe_train_dest, bpe_dev_source, bpe_dev_dest, source_vocab, dest_vocab) = self.__preprocess_corpora(num_merges[0])
-
-         print(bpe_train_source, bpe_train_dest, bpe_dev_source, bpe_dev_dest, source_vocab, dest_vocab)
-
-         #exit(0)
-
-         if SKIP_TRAIN: return self.score_nist(model_path, source_vocab, dest_vocab, bpe_dev_source, num_merges[0]) 
 
          train_command = [ "shell/train_brief.sh",
                           str(self.train_source),
@@ -288,16 +257,10 @@ class Experiment:
                           str(log_path)
                          ]
 
-         #print("Starting training on %d merges" % num_merges[0])
-
          training = subprocess.Popen(train_command, universal_newlines=True)
-         (output, err) = training.communicate()
          status = training.wait()
-
          if status:
              raise RuntimeError("Training process with " + str(num_merges[0]) + " merges failed with exit code " + str(status))
-
-         #print("Finished training on %d merges" % num_merges[0])
 
          return self.score_nist(model_path, source_vocab, dest_vocab, bpe_dev_source, num_merges[0]) 
    
@@ -307,16 +270,9 @@ class Experiment:
                                  [ (0, self.max_merges) ],
                                  n_calls = 10,
                                  random_state = 1,
-                                  verbose = True)
+                                 verbose = self.verbose)
         return res
 
     def run_experiment(self):
         res = self.optimize_merges()
         print(res)
-    
-if __name__ == "__main__":
-    source_lang = sys.argv[1]
-    dest_lang = sys.argv[2]
-
-    exp = Experiment(source_lang, dest_lang)
-    exp.run_experiment()
